f76c007d8ebd1a688d3d9aed79f8ba86ddf15cb5
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-11 19:49:07 +0200

    Añadir controlador

---
+++ Entrega1/FONTS/E1/src/Dominio/ControladorDominio.java
@@ -0,0 +1,9 @@
+package Dominio;
+import  Dominio.*;
+public class ControladorDominio {
+    public static void main(String[] args) {
+         Autor a = new Autor("Joan");
+         System.out.println(a.getName());
+    }
+
+}
