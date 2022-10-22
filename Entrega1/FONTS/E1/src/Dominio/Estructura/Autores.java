a8d1248243b89840cde23c708ae49d1aaab14b89
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-22 17:51:45 +0200

    Cambios en la clase

--- Entrega1/FONTS/E1/src/Dominio/Estructura/Autores.java
+++ Entrega1/FONTS/E1/src/Dominio/Estructura/Autores.java
@@ -5,17 +5,17 @@ import Dominio.Estructura.Autor;
 import java.util.ArrayList;
 
 public class Autores {
-    ArrayList<Autor> Autores = new ArrayList<Autor>();
+    ArrayList<Autor> Autores;
     public Autores (){
         this.Autores = new ArrayList<Autor>();
     }
 
     public void add(Autor a){
-        this.Autores.add(a);
+        Autores.add(a);
     }
 
     public void remove(Autor a){
-        this.Autores.remove(a);
+        Autores.remove(a);
     }
     public ArrayList<Autor> getAutores(){
         return Autores;
