c625bcf40cfdb4d205f311ff2d87235c7c5b317e
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-22 19:33:35 +0200

    getter/setter

--- Entrega1/FONTS/E1/src/Dominio/Expresion/Expresion.java
+++ Entrega1/FONTS/E1/src/Dominio/Expresion/Expresion.java
@@ -1,10 +1,28 @@
 package Dominio.Expresion;
 
 public class Expresion {
+
     private String alias;
     private String expresion;
 
-    public Expresion(String expresion) {
+    public Expresion(String expresion,String alias) {
+        this.expresion = expresion;
+        this.alias = alias;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getExpresion() {
+        return expresion;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public void setExpresion(String expresion) {
         this.expresion = expresion;
     }
 }
