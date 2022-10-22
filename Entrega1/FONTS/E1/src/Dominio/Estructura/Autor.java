bdce4d4626a8fc7778f2faff0b2fb4707a9ba9fd
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-22 16:11:42 +0200

    Añadir javadoc

--- Entrega1/FONTS/E1/src/Dominio/Estructura/Autor.java
+++ Entrega1/FONTS/E1/src/Dominio/Estructura/Autor.java
@@ -1,10 +1,22 @@
 package Dominio.Estructura;
 
+/**
+ * Clase Autor
+ */
 public class Autor {
     private String name;
+
+    /**
+     * @param name
+     * Constructor
+     */
     public Autor (String name){
         this.name = name;
     }
+
+    /**
+     * @return el nombre de el autor
+     */
     public String getName() {
         return name;
     }
