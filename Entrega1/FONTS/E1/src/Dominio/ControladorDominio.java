6c734338e99d06127bbbddfe7ec790295b8cff6f
jamipouchi <miquelpuigturon@gmail.com>
2022-10-23 17:54:34 +0200

    Autores es capaz de gestionar la busqueda por autor (devuelve sus titulos) y la búsqueda por prefijo (devuelve los autores con susodicho prefijo)

--- Entrega1/FONTS/E1/src/Dominio/ControladorDominio.java
+++ Entrega1/FONTS/E1/src/Dominio/ControladorDominio.java
@@ -1,17 +1,32 @@
 package Dominio;
+
 import Dominio.Estructura.Autor;
+import Dominio.Estructura.Titulo;
 import Dominio.Estructura.Autores;
 
 import java.util.ArrayList;
 
 public class ControladorDominio {
     public static void main(String[] args) {
-         Autor a = new Autor("Joan");
-         System.out.println(a.getName());
-         Autores b = new Autores();
-         b.Add(a);
-         ArrayList<Autor> ab = b.getAutores();
-         System.out.println(ab.get(0).getName()) ;
+        Autor a = new Autor("Joan");
+        System.out.println("expect Joan, its a.getName()");
+        System.out.println(a.getName());
+        Autores b = new Autores();
+        b.add(new Autor("Miquel"));
+        b.add(new Autor("za"));
+        b.add(a);
+        Titulo t = new Titulo("Hei");
+        b.addTitleToAutor(t, a);
+        System.out.println("expect the titles, it's b.getTitles()");
+        System.out.println(b.getTitles(a));
+        System.out.println("expect za, its getAutores, con prefijo z");
+        System.out.println(b.getAutores("z"));
+        ArrayList<Autor> ab = b.getAutores();
+        System.out.println("expect los autores, its b.getAutores()");
+        System.out.println(ab);
+        b.remove(a);
+        System.out.println("expect los autores menos Joan, b.remove(a)");
+        System.out.println(b.getAutores());
 
     }
 }
--- Entrega1/FONTS/E1/src/Dominio/Estructura/Autores.java
+++ Entrega1/FONTS/E1/src/Dominio/Estructura/Autores.java
@@ -1,24 +1,65 @@
 package Dominio.Estructura;
 
-import Dominio.Estructura.Autor;
-
 import java.util.ArrayList;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.HashSet;
 
 public class Autores {
-    ArrayList<Autor> Autores = new ArrayList<Autor>();
-    public Autores (){
-        this.Autores = new ArrayList<Autor>();
+    SortedMap<Autor, HashSet<Titulo>> autores;
+
+    public Autores() {
+        this.autores = new TreeMap<>();
     }
 
-    public void Add(Autor a){
-        this.Autores.add(a);
+    public void add(Autor a) {
+        this.autores.put(a, new HashSet<Titulo>());
     }
 
-    public void Remove(Autor a){
-        this.Autores.remove(a);
-    }
-    public ArrayList<Autor> getAutores(){
-        return Autores;
+    public void addTitleToAutor(Titulo t, Autor a) {
+        HashSet<Titulo> titulos = autores.get(a);
+        titulos.add(t);
+        autores.replace(a, titulos);
     }
 
+    public void remove(Autor a) {
+        this.autores.remove(a);
+    }
+
+    public ArrayList<Autor> getAutores() {
+        return new ArrayList<Autor>(autores.keySet());
+    }
+
+    public HashSet<Titulo> getTitles(Autor a) {
+        return autores.get(a);
+    }
+
+    public ArrayList<Autor> getAutores(String prefix) {
+        if (prefix == "") {
+            return new ArrayList<Autor>(autores.keySet());
+        }
+
+        String nextPrefix = getNextPrefix(prefix);
+        if (nextPrefix != null) {
+            return new ArrayList<Autor>(
+                    autores.subMap(new Autor(prefix), new Autor(nextPrefix)).keySet());
+        }
+        return new ArrayList<Autor>(autores.tailMap(new Autor(prefix)).keySet());
+    }
+
+    private String getNextPrefix(String prefix) {
+        for (int charIdx = prefix.length() - 1; charIdx >= 0; charIdx--) {
+            if (prefix.charAt(charIdx) != 'z') {
+                return replaceChar(prefix, charIdx);
+            }
+        }
+        return null;
+    }
+
+    private String replaceChar(String prefix, int idx) {
+        char nextChar = (char) (prefix.charAt(idx) + 1);
+        StringBuilder nextPrefix = new StringBuilder(prefix);
+        nextPrefix.setCharAt(idx, nextChar);
+        return nextPrefix.toString();
+    }
 }
