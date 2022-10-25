d85bc6c21640f7f9a5a3f165c6e5a09d167214b2
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-25 16:56:32 +0200

    Añadir función validar expresion

--- Entrega1/FONTS/E1/src/Dominio/Expresion/Expresion.java
+++ Entrega1/FONTS/E1/src/Dominio/Expresion/Expresion.java
@@ -1,5 +1,8 @@
 package Dominio.Expresion;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Expresion {
 
     private String alias;
@@ -25,4 +28,57 @@ public class Expresion {
     public void setExpresion(String expresion) {
         this.expresion = expresion;
     }
+
+    /**
+     * @param expr Una expresión
+     * @return true si está bien balanceada,
+     * falso en caso contrario
+     */
+    private boolean areBracketsBalanced(String expr)
+    {
+        // Using ArrayDeque is faster than using Stack class
+        Deque<Character> stack = new ArrayDeque<Character>();
+
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++) {
+            char x = expr.charAt(i);
+
+            if (x == '(' || x == '{') {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty()) return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+
+
+
 }
