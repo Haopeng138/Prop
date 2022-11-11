2aca2a24f2a05e36d9389fd84400d2ae505b6e0d
hang.yao <hang.yao@estudiantat.upc.edu>
2022-11-11 10:13:08 +0100

    Completado el driver de busqueda

--- Entrega1/FONTS/E1/src/Dominio/Logica/ControladorBusqueda.java
+++ Entrega1/FONTS/E1/src/Dominio/Logica/ControladorBusqueda.java
@@ -4,22 +4,24 @@ import java.util.*;
 
 import Dominio.Estructura.Autor;
 import Dominio.Estructura.Documento;
+import Dominio.Estructura.Documentos;
 import Dominio.Utils.BinaryTree;
 import Dominio.Utils.ParseNode;
 
 public class ControladorBusqueda {
 
-    public ArrayList<Autor> buscarPorPrefijo(TreeSet<Autor> autores, String prefix) {
+    public static ArrayList<Autor> buscarPorPrefijo(TreeSet<Autor> autores, String prefix) {
         return BusquedaPorPrefijo.buscar(autores, prefix);
     }
 
-    public Set<Documento> buscarPorExpresion(String expresion){
+    public static Set<Documento> buscarPorExpresion(String expresion){
         BinaryTree<ParseNode> bTree = new BinaryTree<ParseNode>();
         Set<Documento> documentos = new HashSet<>();
         return BusquedaPorExpresion.buscar(bTree,documentos);
     }
 
-    public ArrayList<Documento> buscarPorSimilitud(Documento D,int K, ArrayList<ArrayList<Double>> frecResult){
+    public static ArrayList<Documento> buscarPorSimilitud(Documento D, int K){
+        ArrayList<ArrayList<Documentos.InfoModificado>> frecResult = Documentos.getFrecResult();
         return BusquedaPorSimilitud.buscar(D,K,frecResult);
     }
 }
