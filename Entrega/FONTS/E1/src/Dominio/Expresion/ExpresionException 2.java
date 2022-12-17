97d720276dcf169e1d448024a91f00e1e17e200d
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-12-17 19:25:36 +0100

    quitar cosas

--- Entrega/FONTS/E1/src/Dominio/Expresion/ExpresionException 2.java
+++
@@ -1,7 +0,0 @@
-package Dominio.Expresion;
-
-public class ExpresionException extends Exception{
-    public ExpresionException(String msg){
-        super(msg);
-    }
-}
--- Entrega/FONTS/E1/src/Dominio/Utils/BinaryTree 2.java
+++
@@ -1,7 +0,0 @@
-package Dominio.Utils;
-
-public class BinaryTree<T> {
-    public BinaryTree<T> left;
-    public BinaryTree<T> right;
-    public T val;
-}
